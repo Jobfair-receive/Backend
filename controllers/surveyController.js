cfb59fc0c493dc20fad5cbba841249e146718450
Wickhye <jihyesim170@gmail.com>
2023-10-19 23:37:17 +0900

    FIX :: CORS 에러 수정

--- controllers/surveyController.js
+++ controllers/surveyController.js
@@ -1,9 +1,9 @@
 const express = require("express");
-const app = express();
 const router = express.Router();
 
-app.post("/test", async (req, res) => {
+router.post("/", async (req, res) => {
     try {
+
         const surveyData = req.body;
 
         let mbtiRes = '';
@@ -25,50 +25,49 @@ app.post("/test", async (req, res) => {
             P: 0, E: 0
         };
 
-        {surveyData.map((Item, index) => {
-            // mbti
-            if (index < 7) {
-                // 매우 동의 ~ 동의일 때
-                if (Item.value < 3) {
-                    BC[Item.ifYes] += (Item.value % 2) + 1;
-                } else if (Item.value == 3) { // 중립일 때
-                    continued;
-                } else { // 비동의 ~ 매우 비동의일 때
-                    BC[Item.ifNo] += (Item.value % 2) + 1;
+         Object.values(surveyData).forEach((Item,index) => {
+
+           // mbti
+           if (index <7){
+               if(Item.value<3){
+                   BC[Item.IfYes] += (Item.value %2) +1;
+               }else if(Item.value ==3){ 
+                   return;
+               }else{
+                   BC[Item.IfNo] +=(Item.value %2)+1;
+               }
+           }
+
+           //msti 
+           else if(index>=7){
+               if(Item.value<3){
+                   SC[Item.IfYes] +=(Item.value%2)+1;
+               }else if(Item.value==3){
+                  return;
+                }else{
+                    SC[Item.IfNo] +=(Item.value%2)+1;
                 }
-            }
+             }
+         });
 
-            // msti
-            else if (index >= 7) {
-                if (Item.value < 3) {
-                    SC[Item.ifYes] += (Item.value % 2) + 1;
-                } else if (Item.value == 3) {
-                    continued;
-                } else {
-                    SC[Item.ifNo] += (Item.value % 2) + 1;
-                }
-            }
-        })}
+         mbtiRes+=BC['E'] >=BC['I']? 'E':'I';
+         mbtiRes+=BC['S'] >=BC['N']? 'S':'N';
+         mbtiRes+=BC['T'] >=BC['F']? 'T':'F';
+         mbtiRes+=BC['J'] >=BC['P']? 'J':'P';
 
-        mbtiRes += BC['E'] >= BC['I'] ? 'E' : 'I';
-        mbtiRes += BC['S'] >= BC['N'] ? 'S' : 'N';
-        mbtiRes += BC['T'] >= BC['F'] ? 'T' : 'F';
-        mbtiRes += BC['J'] >= BC['P'] ? 'J' : 'P';
+         mstiRes+=SC['S'] >=SC['O']? 'S':'O';
+         mstiRes+=SC['T'] >=SC['A']? 'T':'A';
+         mstiRes+=SC['D' ]>=SC ['C '] ?'D':'C';
+        mstiRes+=SC['P']>=SC['E']?'P':'E';
 
-        mstiRes += SC['S'] >= SC['O'] ? 'S' : 'O';
-        mstiRes += SC['T'] >= SC['A'] ? 'T' : 'A';
-        mstiRes += SC['D'] >= SC['C'] ? 'D' : 'C';
-        mstiRes += SC['P'] >= SC['E'] ? 'P' : 'E';
-
-        const result = {
-            mbti: mbtiRes,
-            msti: mstiRes
-        };
+         const result ={
+             mbti:mbtiRes,
+             msti:mstiRes
+         };
 
         res.status(200).json(result);
     } catch (error) {
         console.log(error);
-        return error;
     }
 })
 
